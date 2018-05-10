1cc766e882b772725a167723088d75c6e5f8665d
Ethan Kravitz <ethan@joinovernight.com>
2018-05-10 10:28:25 -0700

    non-interactive

--- nimiq-client-install.sh
+++ nimiq-client-install.sh
@@ -17,41 +17,20 @@ displayErr() {
   exit 1;
 }
 
-IS_ROOT=false
-if [ "$EUID" -eq 0 ]; then
-  output "WARNING: It looks like you're running as root. It is highly recommended NOT to"
-  output "run miners as root, as you may open yourself to vulnerabilities!"
-
-  while [[ ! $CONTINUE =~ ^[yn]$ ]]; do
-    read -e -s -n 1 -p "Continue as root anyway? [y/N] " CONTINUE
-    if [ -z "$CONTINUE" ]; then
-      CONTINUE='n'
-    else
-      CONTINUE=$(echo "$CONTINUE" | tr '[:upper:]' '[:lower:]')
-    fi
-  done
-
-  if [[ $CONTINUE == "n" ]]; then
-    exit
-  fi
-
-  IS_ROOT=true
-fi
 
 output " "
 output " "
 output "Please double check before hitting enter! You only have one shot at these!"
 output " "
-read -e -p "Enter the pool URL: " -i "us-east.porkypool.com:8444" POOL
-read -e -p "Enter the miner CPU threads: " -i $(getconf _NPROCESSORS_ONLN) THREADS
-read -e -p "Enter your wallet address: " WALLET
-read -e -p "Enter device label: " EXTRADATA
-read -e -p "Enter statistics interval in seconds: " -i "15" STATISTICS
+POOL="us-east.porkypool.com:8444"
+THREADS=$(getconf _NPROCESSORS_ONLN)
+WALLET="NQ03 7EHY 0UFJ 6S6Q 2XAG YHN6 3J22 JM6A 926G"
+EXTRADATA=$(uname -a | awk  '{print $2}')
+STATISTICS=15
 
 output " "
 output "Making sure everything is up to date."
 output " "
-sleep 3
 
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get -y update
@@ -63,39 +42,31 @@ output "Adding nodejs sources."
 output " "
 sleep 3
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 
 output " "
 output "Installing required dependencies."
 output " "
-sleep 3
 
 sudo apt-get install -y git build-essential python2.7 python-dev nodejs unzip
 
 output " "
 output "Downloading Nimiq core."
 output " "
-sleep 3
 
 git clone https://github.com/ryan-rowland/core.git
 
 output " "
 output "Building Nimiq core client."
 output " "
-sleep 3
 
 cd core
-if [ IS_ROOT == true ]; then
-  npm install --unsafe-perm
-else
-  npm install
-fi
+npm install
 npm run prepare
 
 output " "
 output "Building launch scripts."
 output " "
-sleep 3
 
 cd ..
 echo '#!/bin/bash
@@ -110,7 +81,6 @@ chmod u+x start
 output " "
 output "Downloading consensus."
 output " "
-sleep 3
 
 if [ ! -d "./main-full-consensus" ]; then
   wget https://github.com/ryan-rowland/Nimiq-Install-Script/raw/master/main-full-consensus.tar.gz
@@ -123,4 +93,3 @@ output " "
 output "To start the miner type ./start"
 output " "
 output "If you need to change any settings, you can do so by editing the start file."
-sleep 3
